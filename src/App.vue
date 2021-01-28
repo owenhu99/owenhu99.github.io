4d8eb1da076c6f0138f1b89b7b55f8de64adee44
Owen Hu <owenhu99@gmail.com>
2021-01-28 03:11:30 -0500

    Add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <Sidebar />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Sidebar from './components/Sidebar';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Sidebar
   },
 
   data: () => ({
@@ -23,8 +21,19 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: 'Bell MT';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Bell MT W01'), local('BellMT'), url(http://themes.googleusercontent.com/licensed/font?kit=kmIaTfxu8bXVwPRMqRVUug) format('truetype');
+}
 #app {
   background-color: #222222;
   color: white;
+  font-family: 'Bell MT';
+  padding: 8% 15% 10% 15%;
+}
+.theme--dark.v-navigation-drawer:not(.v-navigation-drawer--floating) .v-navigation-drawer__border {
+    background-color: transparent;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-navigation-drawer absolute permanent dark color="transparent" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Xiao Owen Hu
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            胡逍
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item v-for="(text, i) in items" :key="i" link color="white">
+          <v-list-item-content>
+          <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        items: [
+            'projects',
+            'music',
+            'vinyl',
+            'photos',
+            'about',
+            'contact'
+        ]
+    }),
+}
+</script>
+
+<style scoped>
+
+</style>
