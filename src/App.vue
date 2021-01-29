ef7125cae6adc9f12075ec3f6fcc446846957ef3
Owen Hu <owenhu99@gmail.com>
2021-01-28 20:42:05 -0500

    Add photo card for about page

--- src/App.vue
+++ src/App.vue
@@ -30,12 +30,27 @@ export default {
   font-weight: 400;
   src: local('Bell MT W01'), local('BellMT'), url(http://themes.googleusercontent.com/licensed/font?kit=kmIaTfxu8bXVwPRMqRVUug) format('truetype');
 }
+
+html, body {
+  height: 100%;
+}
+
 #app {
+  /* height: 100%;
+  align-items: center;
+  justify-items: center; */
   background-color: #222222;
   color: white;
   font-family: 'Bell MT';
-  padding: 8% 15% 10% 15%;
+  padding: 8% 16% 10% 15%;
 }
+
+.v-application--wrap {
+  min-height: auto;
+  max-width: 840px;
+  flex-direction: row;
+}
+
 .theme--dark.v-navigation-drawer:not(.v-navigation-drawer--floating) .v-navigation-drawer__border {
     background-color: transparent;
 }
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <p>about</p>
+  <v-container>
+    <v-hover v-slot:default="{ hover }">
+      <v-card flat>
+        <v-img src="@/assets/about.jpg" height="500">
+          <v-fade-transition>
+            <v-overlay absolute v-if="hover" color="#999999">
+              <v-card-text>
+                <v-icon dark small>mdi-map-marker</v-icon>
+                Naha, Okinawa
+              </v-card-text>
+            </v-overlay>
+          </v-fade-transition>
+        </v-img>
+      </v-card>
+    </v-hover>
+      <v-card-title>About me</v-card-title>
+
+      <v-card-text>
+        
+      </v-card-text>
+  </v-container>
 </template>
 
 <script>
