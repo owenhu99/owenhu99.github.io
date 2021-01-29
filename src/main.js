7ab60b9d1f63ea2918735db165bc2d0cffbeef7f
Owen Hu <owenhu99@gmail.com>
2021-01-28 23:23:11 -0500

    Add swiper component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import vuetify from './plugins/vuetify'
 import Routes from './router/routes'
+import {Swiper as SwiperClass, Pagination, Navigation, Mousewheel, Autoplay} from 'swiper/swiper.esm'
+import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter'
+
+import 'swiper/swiper-bundle.min.css'
+
+SwiperClass.use([Pagination, Mousewheel, Navigation, Autoplay])
+Vue.use(getAwesomeSwiper(SwiperClass))
+const {Swiper, SwiperSlide} = getAwesomeSwiper(SwiperClass)
 
 Vue.config.productionTip = false
 
@@ -14,6 +22,8 @@ const router = new VueRouter({
 
 new Vue({
   vuetify,
+  Swiper,
+  SwiperSlide,
   render: h => h(App),
   router: router
 }).$mount('#app')
