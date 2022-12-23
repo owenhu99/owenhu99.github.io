69583baa5cd7dfa97d896f8613fb2cc844dfbb36
Owen Hu <owenhu99@gmail.com>
2022-12-22 20:14:04 -0500

    Tweaked Photos.vue

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container style="margin: 20px 20px 20px 20px" class="white">
+  <v-container class="white" style="padding: 1%;">
     <v-card tile height="100%">
       <v-carousel height="100%" hide-delimiters show-arrows-on-hover>
-        <v-carousel-item v-for="(photo,i) in photos" :key="i" height="600px">
+        <v-carousel-item v-for="(photo,i) in photos" :key="i">
           <v-img 
             :src="require('../assets/' + photo + '.jpg')"
             :lazy-src="require('../assets/' + photo + '-lazy.jpg')"
